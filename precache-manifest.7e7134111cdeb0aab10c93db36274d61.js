99a6f73b44fe465508a103180f7f07c3bc597046
sondn272 <63297485+sondn272@users.noreply.github.com>
2020-10-21 19:36:21 +0700

    Updates

---
+++ precache-manifest.7e7134111cdeb0aab10c93db36274d61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a5d9a5d948cd1bd1062fe71141af13",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "7ec4cc6fe93c3ca58c75",
+    "url": "/quote-generator/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4d0126041e5af004bfce",
+    "url": "/quote-generator/static/css/main.d988d174.chunk.css"
+  },
+  {
+    "revision": "7ec4cc6fe93c3ca58c75",
+    "url": "/quote-generator/static/js/2.a0a1ef70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-generator/static/js/2.a0a1ef70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0126041e5af004bfce",
+    "url": "/quote-generator/static/js/main.f0e1f032.chunk.js"
+  },
+  {
+    "revision": "145f8ea2086bb138bc76",
+    "url": "/quote-generator/static/js/runtime-main.515d93d1.js"
+  }
+]);
